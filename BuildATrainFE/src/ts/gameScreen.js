4a35deeb2220986352f933df1d6a34aac8e97e92
Joshua-Wacks <joshuawa@bdd.co.za>
2023-07-22 23:30:16 +0200

    Basic background and train tracks

--- BuildATrainFE/src/ts/gameScreen.js
+++ BuildATrainFE/src/ts/gameScreen.js
@@ -65,112 +65,158 @@ function loadImage(url) {
         image.src = url;
     });
 }
-function drawBackGround() {
+function drawGame() {
     return __awaiter(this, void 0, void 0, function () {
-        var grass1, y, x, vertTrans, sand1, horzTrans, treesB;
+        var gridSize, grass1, y, x, y, x, vertTrans, y, vertTrans2, y, sand1, x, y, x, y, x, horzTrans, y, horzTrans2, x, treesB;
         return __generator(this, function (_a) {
             switch (_a.label) {
-                case 0: return [4 /*yield*/, loadImage('./images/gameGrid/tileGrass1.png')];
+                case 0:
+                    gridSize = 50;
+                    return [4 /*yield*/, loadImage('./images/gameGrid/tileGrass1.png')];
                 case 1:
                     grass1 = _a.sent();
-                    for (y = 4; y < 254; y = y + 50) {
-                        for (x = 4; x < 254; x = x + 50) {
-                            ctx.drawImage(grass1, y, x, 50, 50);
+                    //Top Left Grass
+                    for (y = 4; y < 254; y = y + gridSize) {
+                        for (x = 4; x < 254; x = x + gridSize) {
+                            ctx.drawImage(grass1, y, x, gridSize, gridSize);
                         }
                     }
-                    vertTrans = new Image();
-                    vertTrans.src = './images/gameGrid/tileGrass_transitionE.png';
-                    vertTrans.onload = function () {
-                        for (var y = 4; y < 254; y = y + 50) {
-                            ctx.drawImage(vertTrans, 254, y, 50, 50);
+                    //Right Grass
+                    for (y = 104; y < 404; y = y + gridSize) {
+                        for (x = 604; x < 854; x = x + gridSize) {
+                            ctx.drawImage(grass1, x, y, 53, gridSize);
                         }
-                    };
-                    sand1 = new Image();
-                    sand1.src = './images/gameGrid/tileSand1.png';
-                    sand1.onload = function () {
-                        for (var x = 304; x < 554; x = x + 50) {
-                            for (var y = 4; y < 400; y = y + 50) {
-                                ctx.drawImage(sand1, x, y, 50, 50);
-                            }
+                    }
+                    return [4 /*yield*/, loadImage('./images/gameGrid/tileGrass_transitionE.png')];
+                case 2:
+                    vertTrans = _a.sent();
+                    for (y = 4; y < 254; y = y + gridSize) {
+                        ctx.drawImage(vertTrans, 254, y, gridSize, gridSize);
+                    }
+                    return [4 /*yield*/, loadImage('./images/gameGrid/tileGrass_transitionW.png')];
+                case 3:
+                    vertTrans2 = _a.sent();
+                    // Top right Vertical Transition
+                    for (y = 104; y < 404; y = y + gridSize) {
+                        ctx.drawImage(vertTrans2, 554, y, gridSize, gridSize);
+                    }
+                    return [4 /*yield*/, loadImage('./images/gameGrid/tileSand1.png')];
+                case 4:
+                    sand1 = _a.sent();
+                    for (x = 304; x < 554; x = x + gridSize) {
+                        for (y = 4; y < 400; y = y + gridSize) {
+                            ctx.drawImage(sand1, x, y, gridSize, gridSize);
                         }
-                        //Left Bottom Sand
-                        for (var y = 254; y < 400; y = y + 50) {
-                            for (var x = 4; x < 304; x = x + 50) {
-                                ctx.drawImage(sand1, x, y, 50, 50);
-                            }
+                    }
+                    //top right sand
+                    for (x = 554; x < 854; x = x + gridSize) {
+                        ctx.drawImage(sand1, x, 4, 53, gridSize);
+                    }
+                    ctx.drawImage(sand1, 554, 54, gridSize, gridSize);
+                    //Left Bottom Sand
+                    for (y = 254; y < 400; y = y + gridSize) {
+                        for (x = 4; x < 304; x = x + gridSize) {
+                            ctx.drawImage(sand1, x, y, gridSize, gridSize);
                         }
-                    };
-                    horzTrans = new Image();
-                    horzTrans.src = './images/gameGrid/tileGrass_transitionS.png';
-                    horzTrans.onload = function () {
-                        for (var y = 4; y < 254; y = y + 50) {
-                            ctx.drawImage(horzTrans, y, 254, 50, 50);
-                        }
-                    };
-                    treesB = new Image();
-                    treesB.src = './images/gameGrid/treeBrown_large.png';
-                    treesB.onload = function () {
-                        ctx.drawImage(treesB, 254, 254, 50, 50);
-                    };
+                    }
+                    return [4 /*yield*/, loadImage('./images/gameGrid/tileGrass_transitionS.png')];
+                case 5:
+                    horzTrans = _a.sent();
+                    for (y = 4; y < 254; y = y + gridSize) {
+                        ctx.drawImage(horzTrans, y, 254, gridSize, gridSize);
+                    }
+                    return [4 /*yield*/, loadImage('./images/gameGrid/tileGrass_transitionN.png')];
+                case 6:
+                    horzTrans2 = _a.sent();
+                    for (x = 604; x < 854; x = x + gridSize) {
+                        ctx.drawImage(horzTrans2, x, 54, 53, gridSize);
+                    }
+                    return [4 /*yield*/, loadImage('./images/gameGrid/treeBrown_large.png')];
+                case 7:
+                    treesB = _a.sent();
+                    ctx.drawImage(treesB, 254, 254, gridSize, gridSize);
+                    ctx.drawImage(treesB, 554, 54, gridSize, gridSize);
+                    drawTracks();
                     return [2 /*return*/];
             }
         });
     });
 }
 function drawTracks() {
-    //All Horizontal Tracks
-    var horzTrack = new Image();
-    horzTrack.src = './images/trainTracks/horzTrack.png';
-    horzTrack.onload = function () {
-        for (var x = 4; x < 244; x = x + 30) {
-            ctx.drawImage(horzTrack, x, 65, 30, 30);
-        }
-        for (var x = 214; x > 34; x = x - 30) {
-            ctx.drawImage(horzTrack, x, 214, 30, 30);
-        }
-        for (var x = 64; x < 334; x = x + 30) {
-            ctx.drawImage(horzTrack, x, 334, 30, 30);
-        }
-    };
-    //Vertical Tracks
-    var vertTrack = new Image();
-    vertTrack.src = './images/trainTracks/vertTrack2.png';
-    vertTrack.onload = function () {
-        for (var y = 95; y < 214; y = y + 30) {
-            ctx.drawImage(vertTrack, 244, y, 30, 30);
-        }
-        for (var y = 244; y < 334; y = y + 30) {
-            ctx.drawImage(vertTrack, 34, y, 30, 30);
-        }
-    };
-    //Top Left Turn
-    var tlTurn = new Image();
-    tlTurn.src = './images/trainTracks/topLeftTurn.png';
-    tlTurn.onload = function () {
-        ctx.drawImage(tlTurn, 244, 65, 30, 30);
-    };
-    //Bottom Left Turn
-    var blTurn = new Image();
-    blTurn.src = './images/trainTracks/bottomLeftTurn.png';
-    blTurn.onload = function () {
-        ctx.drawImage(blTurn, 244, 214, 30, 30);
-    };
-    //Top Right Turn
-    var trTurn = new Image();
-    trTurn.src = './images/trainTracks/topRightTurn.png';
-    trTurn.onload = function () {
-        ctx.drawImage(trTurn, 34, 214, 30, 30);
-    };
-    //Bottom Right Turn
-    var brTurn = new Image();
-    brTurn.src = './images/trainTracks/bottomRightTurn.png';
-    brTurn.onload = function () {
-        ctx.drawImage(brTurn, 34, 334, 30, 30);
-    };
-}
-function drawGame() {
-    drawBackGround();
-    drawTracks();
+    return __awaiter(this, void 0, void 0, function () {
+        var trackSize, horzTrack, x, x, x, x, x, x, vertTrack, y, y, y, y, y, tlTurn, blTurn, trTurn, brTurn;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    trackSize = 30;
+                    return [4 /*yield*/, loadImage('./images/trainTracks/horzTrack.png')];
+                case 1:
+                    horzTrack = _a.sent();
+                    for (x = 4; x < 244; x = x + trackSize) {
+                        ctx.drawImage(horzTrack, x, 65, trackSize, trackSize);
+                    }
+                    for (x = 214; x > 34; x = x - trackSize) {
+                        ctx.drawImage(horzTrack, x, 214, trackSize, trackSize);
+                    }
+                    for (x = 64; x < 394; x = x + trackSize) {
+                        ctx.drawImage(horzTrack, x, 334, trackSize, trackSize);
+                    }
+                    for (x = 454; x < 544; x = x + trackSize) {
+                        ctx.drawImage(horzTrack, x, 214, trackSize, trackSize);
+                    }
+                    for (x = 574; x < 664; x = x + trackSize) {
+                        ctx.drawImage(horzTrack, x, 364, trackSize, trackSize);
+                    }
+                    for (x = 694; x < 844; x = x + trackSize) {
+                        ctx.drawImage(horzTrack, x, 94, trackSize, trackSize);
+                    }
+                    ctx.drawImage(horzTrack, 826, 94, trackSize, trackSize);
+                    return [4 /*yield*/, loadImage('./images/trainTracks/vertTrack2.png')];
+                case 2:
+                    vertTrack = _a.sent();
+                    for (y = 95; y < 214; y = y + trackSize) {
+                        ctx.drawImage(vertTrack, 244, y, trackSize, trackSize);
+                    }
+                    for (y = 244; y < 334; y = y + trackSize) {
+                        ctx.drawImage(vertTrack, 34, y, trackSize, trackSize);
+                    }
+                    ctx.drawImage(vertTrack, 394, 364, trackSize, trackSize);
+                    ctx.drawImage(vertTrack, 394, 374, trackSize, trackSize);
+                    for (y = 4; y < 214; y = y + trackSize) {
+                        ctx.drawImage(vertTrack, 424, y, trackSize, trackSize);
+                    }
+                    for (y = 244; y < 364; y = y + trackSize) {
+                        ctx.drawImage(vertTrack, 544, y, trackSize, trackSize);
+                    }
+                    for (y = 334; y > 94; y = y - trackSize) {
+                        ctx.drawImage(vertTrack, 664, y, trackSize, trackSize);
+                    }
+                    return [4 /*yield*/, loadImage('./images/trainTracks/topLeftTurn.png')];
+                case 3:
+                    tlTurn = _a.sent();
+                    ctx.drawImage(tlTurn, 244, 65, trackSize, trackSize);
+                    ctx.drawImage(tlTurn, 394, 334, trackSize, trackSize);
+                    ctx.drawImage(tlTurn, 544, 214, trackSize, trackSize);
+                    return [4 /*yield*/, loadImage('./images/trainTracks/bottomLeftTurn.png')];
+                case 4:
+                    blTurn = _a.sent();
+                    ctx.drawImage(blTurn, 244, 214, trackSize, trackSize);
+                    ctx.drawImage(blTurn, 664, 364, trackSize, trackSize);
+                    return [4 /*yield*/, loadImage('./images/trainTracks/topRightTurn.png')];
+                case 5:
+                    trTurn = _a.sent();
+                    ctx.drawImage(trTurn, 34, 214, trackSize, trackSize);
+                    ctx.drawImage(trTurn, 664, 94, trackSize, trackSize);
+                    return [4 /*yield*/, loadImage('./images/trainTracks/bottomRightTurn.png')];
+                case 6:
+                    brTurn = _a.sent();
+                    ctx.drawImage(brTurn, 34, 334, trackSize, trackSize);
+                    ctx.drawImage(brTurn, 424, 214, trackSize, trackSize);
+                    ctx.drawImage(brTurn, 544, 364, trackSize, trackSize);
+                    return [2 /*return*/];
+            }
+        });
+    });
 }
 setupComponents();
 drawGame();
--- BuildATrainFE/src/ts/gameScreen.ts
+++ BuildATrainFE/src/ts/gameScreen.ts
@@ -37,139 +37,188 @@ function loadImage(url:string):Promise<CanvasImageSource>
       });
 }
 
-async function drawBackGround()
+async function drawGame()
 {
-    //Top Left Grass
-    let grass1:CanvasImageSource = await loadImage('./images/gameGrid/tileGrass1.png');
 
-        for (let y = 4; y < 254; y=y+50) 
+    let gridSize: number = 50;
+    let grass1:CanvasImageSource = await loadImage('./images/gameGrid/tileGrass1.png');
+        //Top Left Grass
+        for (let y = 4; y < 254; y=y+gridSize) 
         {
-            for (let x = 4; x < 254; x=x+50) 
+            for (let x = 4; x < 254; x=x+gridSize) 
             {
-                ctx.drawImage(grass1, y, x,50,50);
+                ctx.drawImage(grass1, y, x,gridSize,gridSize);
+            }
+        }
+
+        //Right Grass
+        for (let y = 104; y < 404; y=y+gridSize) 
+        {
+            for (let x = 604; x < 854; x=x+gridSize) 
+            {
+                ctx.drawImage(grass1, x, y,53,gridSize);
             }
         }
 
     //First Vertical Transition
     let vertTrans:CanvasImageSource = await loadImage('./images/gameGrid/tileGrass_transitionE.png');
-    for (let y = 4; y < 254; y=y+50) 
-    {
-        ctx.drawImage(vertTrans, 254, y,50,50);
-    }
-
-    //Middle sand
-    let sand1 = new Image();
-    sand1.src = './images/gameGrid/tileSand1.png';
-    sand1.onload = () => {
-        for (let x = 304; x < 554; x=x+50) 
+        for (let y = 4; y < 254; y=y+gridSize) 
         {
-            for (let y = 4; y < 400; y=y+50) 
+            ctx.drawImage(vertTrans, 254, y,gridSize,gridSize);
+        }
+
+
+    let vertTrans2:CanvasImageSource = await loadImage('./images/gameGrid/tileGrass_transitionW.png');
+        // Top right Vertical Transition
+        for (let y = 104; y < 404; y=y+gridSize) 
+        {
+            ctx.drawImage(vertTrans2, 554, y,gridSize,gridSize);
+        }
+
+
+    //middle sand
+    let sand1:CanvasImageSource = await loadImage('./images/gameGrid/tileSand1.png');
+
+        for (let x = 304; x < 554; x=x+gridSize) 
+        {
+            for (let y = 4; y < 400; y=y+gridSize) 
             {
-                ctx.drawImage(sand1, x, y,50,50);
+                ctx.drawImage(sand1, x, y,gridSize,gridSize);
             }
         }
+
+        //top right sand
+        for (let x = 554; x < 854; x=x+gridSize) 
+        {
+            ctx.drawImage(sand1, x, 4,53,gridSize);
+        }
+        ctx.drawImage(sand1, 554, 54,gridSize,gridSize);
 
         //Left Bottom Sand
-        for (let y = 254; y < 400; y=y+50) 
+        for (let y = 254; y < 400; y=y+gridSize) 
         {
-            for (let x = 4; x < 304; x=x+50) 
+            for (let x = 4; x < 304; x=x+gridSize) 
             {
-                ctx.drawImage(sand1, x, y,50,50);
+                ctx.drawImage(sand1, x, y,gridSize,gridSize);
             }
         }
-    };
+
 
     //First Horizontal Transition
-    let horzTrans = new Image();
-    horzTrans.src = './images/gameGrid/tileGrass_transitionS.png';
-    horzTrans.onload = () => {
-        for (let y = 4; y < 254; y=y+50) 
+    let horzTrans:CanvasImageSource = await loadImage('./images/gameGrid/tileGrass_transitionS.png');
+        for (let y = 4; y < 254; y=y+gridSize) 
         {
-            ctx.drawImage(horzTrans, y, 254,50,50);
+            ctx.drawImage(horzTrans, y, 254,gridSize,gridSize);
         }
-    }
+
+        //Top right Horizontal Transition
+        let horzTrans2:CanvasImageSource = await loadImage('./images/gameGrid/tileGrass_transitionN.png');
+
+        for (let x = 604; x < 854; x=x+gridSize) 
+        {
+            ctx.drawImage(horzTrans2, x, 54,53,gridSize);
+        }
+
+
 
     //Twigs or trees
-    let treesB = new Image();
-    treesB.src = './images/gameGrid/treeBrown_large.png';
-    treesB.onload = () => {
-        ctx.drawImage(treesB, 254, 254,50,50);
+    let treesB:CanvasImageSource = await loadImage('./images/gameGrid/treeBrown_large.png');
+    ctx.drawImage(treesB, 254, 254,gridSize,gridSize);
+    ctx.drawImage(treesB, 554, 54,gridSize,gridSize);
 
-    }
+    drawTracks();
+
 }
 
-function drawTracks()
+async function drawTracks()
 {
+    let trackSize: number = 30;
      //All Horizontal Tracks
-     let horzTrack = new Image();
-     horzTrack.src = './images/trainTracks/horzTrack.png';
-     horzTrack.onload = () => {
-         for (let x = 4; x < 244; x=x+30) 
-         {
-             ctx.drawImage(horzTrack, x, 65,30,30);
-         }
- 
-         for (let x = 214; x > 34; x=x-30) 
-         {
-             ctx.drawImage(horzTrack, x, 214,30,30);
-         }
- 
-         for (let x = 64; x < 334; x=x+30) 
-         {
-             ctx.drawImage(horzTrack, x, 334,30,30);
-         }
-     }
- 
+    let horzTrack:CanvasImageSource = await loadImage('./images/trainTracks/horzTrack.png');
+        for (let x = 4; x < 244; x=x+trackSize) 
+        {
+            ctx.drawImage(horzTrack, x, 65,trackSize,trackSize);
+        }
+
+        for (let x = 214; x > 34; x=x-trackSize) 
+        {
+            ctx.drawImage(horzTrack, x, 214,trackSize,trackSize);
+        }
+
+        for (let x = 64; x < 394; x=x+trackSize) 
+        {
+            ctx.drawImage(horzTrack, x, 334,trackSize,trackSize);
+        }
+
+        for (let x = 454; x < 544; x=x+trackSize) 
+        {
+            ctx.drawImage(horzTrack, x, 214,trackSize,trackSize);
+        }
+
+        for (let x = 574; x < 664; x=x+trackSize) 
+        {
+            ctx.drawImage(horzTrack, x, 364,trackSize,trackSize);
+        }
+
+        for (let x = 694; x < 844; x=x+trackSize) 
+        {
+            ctx.drawImage(horzTrack, x, 94,trackSize,trackSize);
+        }
+        ctx.drawImage(horzTrack, 826, 94,trackSize,trackSize);
+
+
      //Vertical Tracks
-     let vertTrack = new Image();
-     vertTrack.src = './images/trainTracks/vertTrack2.png';
-     vertTrack.onload = () => {
-         for (let y = 95; y < 214; y=y+30) 
-         {
-             ctx.drawImage(vertTrack, 244, y,30,30);
-         }
- 
-         for (let y = 244; y < 334; y=y+30) 
-         {
-             ctx.drawImage(vertTrack, 34, y,30,30);
-         }
-     }
- 
+    let vertTrack:CanvasImageSource = await loadImage('./images/trainTracks/vertTrack2.png');
+        for (let y = 95; y < 214; y=y+trackSize) 
+        {
+            ctx.drawImage(vertTrack, 244, y,trackSize,trackSize);
+        }
+
+        for (let y = 244; y < 334; y=y+trackSize) 
+        {
+            ctx.drawImage(vertTrack, 34, y,trackSize,trackSize);
+        }
+        ctx.drawImage(vertTrack, 394, 364,trackSize,trackSize);
+        ctx.drawImage(vertTrack, 394, 374,trackSize,trackSize);
+
+        for (let y = 4; y < 214; y=y+trackSize) 
+        {
+            ctx.drawImage(vertTrack, 424, y,trackSize,trackSize);
+        }
+
+        for (let y = 244; y < 364; y=y+trackSize) 
+        {
+            ctx.drawImage(vertTrack, 544, y,trackSize,trackSize);
+        }
+
+        for (let y = 334; y > 94; y=y-trackSize) 
+        {
+            ctx.drawImage(vertTrack, 664, y,trackSize,trackSize);
+        }
+
+
      //Top Left Turn
-     let tlTurn = new Image();
-     tlTurn.src = './images/trainTracks/topLeftTurn.png';
-     tlTurn.onload = () => {
-         ctx.drawImage(tlTurn, 244, 65,30,30);
-     }
- 
+    let tlTurn:CanvasImageSource = await loadImage('./images/trainTracks/topLeftTurn.png');
+        ctx.drawImage(tlTurn, 244, 65,trackSize,trackSize);
+        ctx.drawImage(tlTurn, 394, 334,trackSize,trackSize);
+        ctx.drawImage(tlTurn, 544, 214,trackSize,trackSize);
+
      //Bottom Left Turn
-     let blTurn = new Image();
-     blTurn.src = './images/trainTracks/bottomLeftTurn.png';
-     blTurn.onload = () => {
-         ctx.drawImage(blTurn, 244, 214,30,30);
-     }
+    let blTurn:CanvasImageSource = await loadImage('./images/trainTracks/bottomLeftTurn.png');
+        ctx.drawImage(blTurn, 244, 214,trackSize,trackSize);
+        ctx.drawImage(blTurn, 664, 364,trackSize,trackSize);
  
      //Top Right Turn
-     let trTurn = new Image();
-     trTurn.src = './images/trainTracks/topRightTurn.png';
-     trTurn.onload = () => {
-         ctx.drawImage(trTurn, 34, 214,30,30);
-     }
+    let trTurn:CanvasImageSource = await loadImage('./images/trainTracks/topRightTurn.png');
+        ctx.drawImage(trTurn, 34, 214,trackSize,trackSize);
+        ctx.drawImage(trTurn, 664, 94,trackSize,trackSize);
  
      //Bottom Right Turn
-     let brTurn = new Image();
-     brTurn.src = './images/trainTracks/bottomRightTurn.png';
-     brTurn.onload = () => {
-         ctx.drawImage(brTurn, 34, 334,30,30);
-     }
-}
+    let brTurn:CanvasImageSource = await loadImage('./images/trainTracks/bottomRightTurn.png');
+        ctx.drawImage(brTurn, 34, 334,trackSize,trackSize);
+        ctx.drawImage(brTurn, 424, 214,trackSize,trackSize);
+        ctx.drawImage(brTurn, 544, 364,trackSize,trackSize);
 
-function drawGame()
-{
-    
-    drawBackGround();
-    drawTracks();
-   
 }
 setupComponents();
 drawGame();
